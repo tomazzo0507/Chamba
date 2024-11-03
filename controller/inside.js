172cecd156822baeca61a898ba9d57db6380a8c2
tomazzo0507 <vargasgiraldotomas@gmail.com>
2024-11-03 18:39:02 -0500

    al ingresar sidebar small

--- controller/inside.js
+++ controller/inside.js
@@ -1,7 +1,8 @@
 var btn = document.querySelector("#btn")
 var sidebar = document.querySelector(".sidebar")
 
+sidebar.classList.add("small");
+
 btn.addEventListener("click", () => {
     sidebar.classList.toggle("small");
-
 })
