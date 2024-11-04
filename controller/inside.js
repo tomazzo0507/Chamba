ad89636451de77375035bc60cb26d08f63f6f02b
KaletJosue <kalet.jo28@gmail.com>
2024-11-03 21:33:32 -0500

    interfaz corregida, interfaz filtro, responsive

--- controller/inside.js
+++ controller/inside.js
@@ -1,8 +1,50 @@
 var btn = document.querySelector("#btn")
 var sidebar = document.querySelector(".sidebar")
 
+var close = document.querySelector('#close')
+var x = document.querySelector('#x')
+
 sidebar.classList.add("small");
 
 btn.addEventListener("click", () => {
     sidebar.classList.toggle("small");
+    filtro.classList.remove('active')
+    btnFilter.classList.remove('active')
+})
+
+close.addEventListener('click', () => {
+    sidebar.classList.add("active");
+    btnFilter.classList.remove('active')
+    filtro.classList.remove('active')
+})
+
+x.addEventListener('click', () => {
+    sidebar.classList.remove("active");
+})
+
+// Dark Mode
+
+const body = document.body
+const checkbox = document.querySelector('.theme-switch__checkbox')
+const palanca = document.querySelector('.theme-switch__checkbox')
+
+palanca.addEventListener('click', () => {
+    body.classList.toggle('dark-mode')
+})
+
+// filter 
+
+var btnFilter = document.querySelector('#filter')
+var filtro = document.querySelector('.filtro')
+
+var close_filtro = document.querySelector('#close_filtro')
+
+btnFilter.addEventListener('click', () => {
+    filtro.classList.toggle('active')
+    btnFilter.classList.toggle('active')
+})
+
+close_filtro.addEventListener('click', () => {
+    filtro.classList.remove('active')
+    btnFilter.classList.remove('active')
 })
--- controller/login.js
+++ controller/login.js
@@ -14,5 +14,19 @@ login.addEventListener("click", () =>  {
     }
 })
 
+// eye
+
+var eye = document.querySelector("#eye")
+
+eye.addEventListener('click', () => {
+    eye.classList.toggle('active')
+
+    if (contra.type == "password") {
+        contra.type = "text"
+    } else if (contra.type == "text") {
+        contra.type = "password"
+    }
+})
+
 
 
